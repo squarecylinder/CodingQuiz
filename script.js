31f496e4384e03bb1ebbd95c358c62d9955ce96f
Square Cylinder <ziencck@gmail.com>
2020-03-12 18:15:24 -0400

    just updating files to dl on my desktop

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 var timer = 120;
-var score = 0;
-var questionList = [];
-var answerList = [];
 
+startTimer();
+
+var score = 0;
+// var questionList = [
+//     {
+//         question: "The correct answer is b",
+//         answers: {
+//             a: "Wrong",
+//             b: "Right",
+//             c: "Wrong"
+//         },
+//         correctAnswer: "b"
+//     },
+//     {
+//         question: "The correct answer is a",
+//         answers: {
+//             a: "Right",
+//             b: "Wrong",
+//             c: "Wrong"
+//         },
+//         correctAnswer: "a"
+//     },
+//     {
+//         question: "The correct answer is c",
+//         answers: {
+//             a: "wrong",
+//             b: "wrong",
+//             c: "Right"
+//         },
+//         correctAnswer: "c"
+//     }
+// ];
+setScore();
+// askQuestion();
 function startTimer() {
     var timerInterval = setInterval(function(){
         timer--;
@@ -15,15 +46,14 @@ function startTimer() {
 }
 function sendMessage(){
     $("#timer").text("Times Up!");
+    $("#initial-form").attr("style", "display:block");
 }
 function setScore() {
     $("#score").text("Score: " + score);
 }
-function askQuestion() {
-    for(var i = 0; i < questionList.length; i++) {
-        $("#quiz-question").text(questionList[i])
-    }
-}
-
-startTimer();
-setScore();
+// function askQuestion() {
+//     for(var i = 0; i < questionList.length; i++) {
+//         $("#quiz-question").text("test");
+//         console.log(JSON.stringify(questionList));
+//     }
+// }
